229539d77f2af04753bc032e18ebc86b04781d6c
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2022-02-04 15:18:58 -0500

    MongoDB: Almacenar y leer datos

---
+++ components/message/model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const myShema = new Schema({
+    user : String,
+    message : {
+        type : String,
+        required: true,
+    },
+    date : Date,
+});
+
+const model = mongoose.model('Message', myShema);
+
+module.exports = model;
--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,24 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+
+//  MatiusRock1  Sm4DCZhEicueY4Yb
+
+//
+db.Promise = global.Promise;
+db.connect('mongodb+srv://MatiusRock1:Sm4DCZhEicueY4Yb@cluster0.lva38.mongodb.net/telegram?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+});
+console.log('[db]Conectada con exito');
 
 function addMessage(message){
-    list.push(message);
+    //list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessage(){
-    return list;
+async function getMessage(){
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
