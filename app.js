97109ff2bf271b7f82610d2d441150d9d8aa1cba
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2022-02-07 10:02:51 -0500

    creacion de socket

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const dotenv = require('dotenv');
 const path = require('path');
 const express = require('express');
+const app = express();
+const server = require('http').Server(app);
 const bodyParser = require ('body-parser');
 const db = require('./db');
 
+//const io = require('socket.io');
+
 const { ok } = require('assert');
 const router = require('./network/routes');
+const { isObject } = require('util');
+
 
 //configuracion de variables de entorno
 dotenv.config({
@@ -16,16 +22,25 @@ db.connectMongoDB(process.env.Mondodb)
 
 
 
-var app = express();
+
+//configuracion de socket
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+    console.log("nuevo cliente cocnectado");
+    socket.emit('mensaje','bienvenido');
+});
+setInterval(function () {
+    io.emit('mensaje','hola os escribo a todos');
+}, 3000)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-//app.use(router);
-
 router(app);
 
-
-
-app.listen(3000);
+server.listen(3000);
+/*server.listen(8080, function(){
+    console.log('Servidor iniciado en http:localhost:8080')
+})*/
 
 
 
